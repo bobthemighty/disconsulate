2c6e6a44b56c7297340c873557f9224cce1ea7e3
Bob Gregory <bob@made.com>
2018-11-28 10:15:49 +0000

    Added support for node-meta, dc, and tags

--- test/index.js
+++ test/index.js
@@ -33,6 +33,67 @@ describe("getService", async () => {
   });
 });
 
+describe("When specifying additional options", () => {
+  let request = null;
+
+  const server = Http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    request = req;
+    res.end(
+      JSON.stringify([{ Service: { Address: "configured.com", Port: "1234" } }])
+    );
+  });
+
+  before(async () => {
+    await server.listen(0);
+    process.env.CONSUL_ADDR = `http://localhost:${server.address().port}`;
+    const client = new Disconsulate();
+    await client.getService("baz", {
+      tags: ["active", "release-123"],
+      dc: "eu-west-1"
+    });
+  });
+
+  it("calls the health endpoint", () => {
+    expect(request.url).to.equal("/v1/health/service/baz?passing=1&near=agent&dc=eu-west-1&tag=active&tag=release-123");
+  });
+
+
+});
+
+describe("When specifying node metadata", () => {
+  let request = null;
+
+  const server = Http.createServer((req, res) => {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    request = req;
+    res.end(
+      JSON.stringify([{ Service: { Address: "configured.com", Port: "1234" } }])
+    );
+  });
+
+  before(async () => {
+    await server.listen(0);
+    process.env.CONSUL_ADDR = `http://localhost:${server.address().port}`;
+    const client = new Disconsulate();
+    await client.getService("baz", {
+      node: {
+        availabilityZone: "A",
+        type: "t2.micro",
+      }
+    });
+  });
+
+  it("calls the health endpoint", () => {
+    expect(request.url).to.equal("/v1/health/service/baz?passing=1&near=agent&node-meta=availabilityZone:A&node-meta=type:t2.micro");
+  });
+
+
+});
+
+
+
+
 describe("When using environment variables", async () => {
   let request = null;
 
